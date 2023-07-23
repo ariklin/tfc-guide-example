4632f0d03c109fc126b2f59cadd80ee6b87967de
ariklin <ariklin@microsoft.com>
2023-07-23 15:52:11 +0300

    3

--- main.tf
+++ main.tf
@@ -2,6 +2,27 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_eks_cluster" "existing_cluster" {
-  name = "lil-eks"
+locals {
+  cluster_name = "lil-eks"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+
+  name = "vpc-0ca5b3e972e21fd68"
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
 }
--- versions.tf
+++ versions.tf
@@ -2,17 +2,42 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
+
+  # cloud {
+  #   workspaces {
+  #     name = "tfc-guide-example"
+  #   }
+  # }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 4.47.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "3.0.0"
+      version = "~> 3.4.3"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.16.1"
+    }
+
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
     }
   }
-
-  required_version = ">= 0.14.0"
 }
